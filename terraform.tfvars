1364290e7c4903d34d1d1ea13c687a164ea7692c
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-22 09:33:52 +0000

    gl

--- terraform.tfvars
+++ terraform.tfvars
@@ -4,3 +4,6 @@ tags = {
   owner        = "ecom_bu"
   cost_center  = 1001
 }
+
+default_vpc_id      = "vpc-05260d90c4efdfa24"
+default_route_table = "rtb-0ee41c8af0bf17bb8"
--- vars.tf
+++ vars.tf
@@ -1,4 +1,9 @@
 variable "env" {}
 variable "tags" {}
-
+variable "default_vpc_id" {}
+variable "default_route_table" {}
 variable "vpc" {}
+variable "docdb" {}
+variable "rds" {}
+variable "elasticache" {}
+variable "rabbitmq" {}
