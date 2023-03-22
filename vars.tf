911ab5de8a9917b7cf859d7e3cbec36b757c1644
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-22 09:41:11 +0000

    var

--- vars.tf
+++ vars.tf
@@ -3,7 +3,3 @@ variable "tags" {}
 variable "default_vpc_id" {}
 variable "default_route_table" {}
 variable "vpc" {}
-variable "docdb" {}
-variable "rds" {}
-variable "elasticache" {}
-variable "rabbitmq" {}
