86f4317c7c5d2c00c48cee41fb9e88abe9ac1820
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-21 02:10:14 +0000

    d

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ module "alb" {
 
 module "app" {
 
-  depends_on = [module.docdb, module.rds, module.elasticache, module.alb, module.rabbitmq]
+  //depends_on = [module.docdb, module.rds, module.elasticache, module.alb, module.rabbitmq]
 
   source           = "git::https://github.com/vlnaresh28/tf-module-app.git"
   env              = var.env
